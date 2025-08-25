8051e4153e52e7f2274f650a620e63d71cff8c4d
GJoyceArpana <arpanajoyce@gmail.com>
2025-08-25 10:30:23 +0530

    updated

--- scope_official_webapp_nextjs/scope_official_web_app/lib/firebase.ts
+++ scope_official_webapp_nextjs/scope_official_web_app/lib/firebase.ts
@@ -1,17 +1,26 @@
-import { initializeApp } from 'firebase/app';
+
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDnEBGDyW5OvPZcgKbZnfxRGiVZyJReOwQ",
+  authDomain: "scope-website-a1f46.firebaseapp.com",
+  projectId: "scope-website-a1f46",
+  storageBucket: "scope-website-a1f46.firebasestorage.app",
+  messagingSenderId: "540664957632",
+  appId: "1:540664957632:web:2b4c8b2a9affdf09013b25",
+  measurementId: "G-PBEXVR0SYG"
 };
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
