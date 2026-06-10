e7f046ce0a93b05d9c4e96b42ab876fa470ad2cc
GJoyceArpana <arpanajoyce@gmail.com>
2026-06-10 22:59:10 +0530

    updated

--- scope_official_webapp_nextjs/scope_official_web_app/app/admin/page.tsx
+++ scope_official_webapp_nextjs/scope_official_web_app/app/admin/page.tsx
@@ -7,6 +7,7 @@ import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 export default async function AdminPage() {
-  const token = (await cookies()).get('admin_token')?.value
+  const cookieStore = await cookies()
+  const token = cookieStore.get('admin_token')?.value
   redirect(token ? '/admin/dashboard' : '/admin/login')
 }
